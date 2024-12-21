63c6599316a42c0bd30a1801121003710b9b6c5e
itzjustfitb <fakhrigajar@gmail.com>
2024-12-22 00:49:23 +0400

    faq section was updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
 import Services from "../../components/Services/Services";
-import FAQ from "../../components/FAQ/FAQ.JSX";
+import FAQ from "../../components/FAQ/FAQ";
 
 function Home() {
   return (
